e5b9cc60c998f51df27e933a971df01e5d4f7f97
Jamie Wong <phleet@gmail.com>
2015-08-21 15:26:54 -0700

    Clean up python example code
    - Reformatted 4 spaces (as per https://www.python.org/dev/peps/pep-0008/#indentation)
    - Remove unnecessary semicolon
    - Remove unused import pyDes

--- python/example.py
+++ python/example.py
@@ -16,38 +16,37 @@
 import json
 import base64, urllib, datetime
 from time import mktime
-from pyDes import *
 from Crypto.Cipher import AES
 
 def get_user_data(projects, registered):
-  data = {
-    'user_id': "12345678901",
-    'login': "johndoe",
-    'user_email': "john.doe@mail.com",
-    'display_name': "John Doe",
-    'locale': "en_US",
-    'gender': 1,
-    'projects': ",".join(projects),
-    'expiration': mktime((datetime.datetime.now() + datetime.timedelta(minutes=20)).timetuple()),
-    'languages': "uk,ro,fr",
-    'role': 0,
-    'redirect_to': "https://crowdin.com/project/docx-project"
-  }
+    data = {
+        'user_id': "12345678901",
+        'login': "johndoe",
+        'user_email': "john.doe@mail.com",
+        'display_name': "John Doe",
+        'locale': "en_US",
+        'gender': 1,
+        'projects': ",".join(projects),
+        'expiration': mktime((datetime.datetime.now() + datetime.timedelta(minutes=20)).timetuple()),
+        'languages': "uk,ro,fr",
+        'role': 0,
+        'redirect_to': "https://crowdin.com/project/docx-project"
+    }
 
-  return data;
+    return data
 
 def encrypt(data, api_key):
-  iv = api_key[16:32]
-  api_key = api_key[0:16]
-  data = json.dumps(data)
-  length = 16 - (len(data) % 16)
-  data += chr(length)*length
+    iv = api_key[16:32]
+    api_key = api_key[0:16]
+    data = json.dumps(data)
+    length = 16 - (len(data) % 16)
+    data += chr(length) * length
 
-  encryptor = AES.new(api_key, AES.MODE_CBC, iv)
-  d = encryptor.encrypt(data)
+    encryptor = AES.new(api_key, AES.MODE_CBC, iv)
+    d = encryptor.encrypt(data)
 
-  base64enc = base64.b64encode(d)
-  return urllib.pathname2url(base64enc)
+    base64enc = base64.b64encode(d)
+    return urllib.pathname2url(base64enc)
 
 basepath = "https://crowdin.com/join"
 owner_login = " -- OWNERS LOGIN -- "
@@ -57,4 +56,4 @@ hash_part = encrypt(get_user_data(projects, False), api_key)
 link = "%s?h=%s&uid=%s" % (basepath, hash_part, owner_login)
 
 if len(link) > 2000:
-  raise Exception("Link is too long.")
+    raise Exception("Link is too long.")
