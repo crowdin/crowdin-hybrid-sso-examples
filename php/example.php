ac9eb16c909b866b8ee96be7cdee122db09ef000
wolf777 <borkovskyj@gmail.com>
2019-08-20 14:15:44 +0300

    use openssl_encrypt function
    use openssl_encrypt function instead of mcrypt_encrypt

--- php/example.php
+++ php/example.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2014 Crowdin LLC
+ * Copyright 2019 Crowdin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,53 +15,60 @@
  * limitations under the License.
  */
 
-class crowdin_custom_sso {
-  // Crowdin account credentials
-  const CROWDIN_USER_LOGIN = ' -- OWNERS LOGIN -- ';      // your login name in Crowdin
-  const CROWDIN_USER_API_KEY = ' -- OWNERS API KEY -- ';  // your account API key (can be found here https://crowdin.net/settings#api-key)
-  const CROWDIN_BASEPATH = 'https://crowdin.com/join';    // usually no need to change
+class CrowdinCustomSso
+{
+    // Crowdin account credentials
+    private const CROWDIN_USER_LOGIN = ' -- OWNERS LOGIN -- ';      // your login name in Crowdin
+    private const CROWDIN_USER_API_KEY = ' -- OWNERS API KEY -- ';  // your account API key (can be found here https://crowdin.com/settings#api-key)
+    private const CROWDIN_BASEPATH = 'https://crowdin.com/join';    // usually no need to change
 
-  private $cipher = MCRYPT_RIJNDAEL_128;
-  private $mode = MCRYPT_MODE_CBC;
+    private static $cipher = 'aes-128-cbc';
 
-  public function get_sso_link() {
-    $hash = $this->encrypt($this->get_user_data());
+    public function getSsoLink(): string
+    {
+        $hash = $this->encrypt($this->getUserData());
 
-    $link = sprintf('%s?h=%s&uid=%s', self::CROWDIN_BASEPATH, $hash, self::CROWDIN_USER_LOGIN);
+        $link = sprintf('%s?h=%s&uid=%s', self::CROWDIN_BASEPATH, $hash, self::CROWDIN_USER_LOGIN);
 
-    if(strlen($link) > 2000) {
-      throw new Exeption('Link is too long.');
+        if (strlen($link) > 2000) {
+            throw new Exeption('Link is too long.');
+        }
+
+        return $link;
     }
 
-    return $link;
-  }
+    private function getUserData(): array
+    {
+        return [
+            'user_id' => '12345678901',
+            'login' => 'JohnDoe',
+            'user_email' => 'john.doe@mail.com',
+            'display_name' => 'John Doe',
+            'locale' => 'en_US',
+            'gender' => 1,
+            'languages' => 'uk,ro,fr',
+            'role' => 0,
+            'projects' => implode(',', ['docx-project', 'csv-project']),
+            'expiration' => strtotime('+20 minutes'),
+            'redirect_to' => 'https://crowdin.com/project/docx-project'
+        ];
+    }
 
-  private function get_user_data() {
-    return array(
-      'user_id' => '12345678901',
-      'login' => 'JohnDoe',
-      'user_email' => 'john.doe@mail.com',
-      'display_name' => 'John Doe',
-      'locale' => 'en_US',
-      'gender' => 1,
-      'languages' => 'uk,ro,fr',
-      'role' => 0,
-      'projects' => implode(',', array('docx-project', 'csv-project')),
-      'expiration' => strtotime('+20 minutes'),
-      'redirect_to' => 'https://crowdin.com/project/docx-project'
-    );
-  }
+    private function encrypt(array $parameters): string
+    {
+        $key = substr(self::CROWDIN_USER_API_KEY, 0, 16);
+        $iv = substr(self::CROWDIN_USER_API_KEY, 16, 16);
 
-  private function encrypt($parameters) {
-    $block_size = mcrypt_get_block_size($this->cipher, $this->mode);
-    $key = substr(self::CROWDIN_USER_API_KEY, 0, 16);
-    $iv = substr(self::CROWDIN_USER_API_KEY, 16, 16);
+        $data = json_encode($parameters);
 
-    $data = json_encode($parameters);
-    $padding = $block_size - (strlen($data) % $block_size);
+        $encryptedData = openssl_encrypt(
+            $data,
+            self::$cipher,
+            $key,
+            OPENSSL_RAW_DATA,
+            $iv
+        );
 
-    $encrypted_data = mcrypt_encrypt($this->cipher, $key, $data . str_repeat(chr($padding), $padding), $this->mode, $iv);
-
-    return urlencode(base64_encode($encrypted_data));
-  }
+        return urlencode(base64_encode($encryptedData));
+    }
 }
